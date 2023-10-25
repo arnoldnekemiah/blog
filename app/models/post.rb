0aae1a12427668fde23f3f09154f44a7b0edb829
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-10-25 18:16:55 +0300

    Added posts association

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,5 +1,11 @@
 class Post < ApplicationRecord
-  has_many :comments
-  belongs_to :user, foreign_key: 'author_id'
-  has_many :likes
+  belongs_to :user
+
+  after_create do
+    user.increment!(:posts_counter)
+  end
+
+  after_destroy do
+    user.decrement!(:posts_counter)
+  end
 end
