cbbd680bd8a495e9df9736ad62458505122e287b
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-10-25 17:41:50 +0300

    Added likes and comments columns to posts table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_10_25_133020) do
+ActiveRecord::Schema[7.1].define(version: 2023_10_25_143705) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -38,6 +38,8 @@ ActiveRecord::Schema[7.1].define(version: 2023_10_25_133020) do
     t.text "text"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "likes_counter"
+    t.integer "comments_counter"
     t.index ["author_id"], name: "index_posts_on_author_id"
   end
 
