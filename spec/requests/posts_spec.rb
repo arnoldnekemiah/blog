81bf9877dc0a9cf412fdea2db6d8735aa0fdce93
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-11-01 18:55:18 +0300

    added tests for request specs

--- spec/requests/posts_spec.rb
+++ spec/requests/posts_spec.rb
@@ -1,7 +1,27 @@
 require 'rails_helper'
 
 RSpec.describe 'Posts', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+  context 'GET /index' do
+    let(:valid_user_id) { 1 } # Replace with a valid user ID
+
+    before :each do
+      get "/users/#{valid_user_id}/posts"
+    end
+
+    it 'returns a successful response' do
+      expect(response).to be_successful
+    end
+
+    it 'returns HTTP status 200' do
+      expect(response.status).to eq(200)
+    end
+
+    it 'renders the right view file' do
+      expect(response).to render_template('posts/index')
+    end
+
+    it 'renders the right placeholder' do
+      expect(response.body).to include('<h1>Here is a list of posts for a given user</h1>')
+    end
   end
 end
