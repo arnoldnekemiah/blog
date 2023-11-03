8ceaf5d8d48c30716092c0d46fa75c57b8c07690
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-11-03 21:42:43 +0300

    fixed linter errors as requested by reviewer

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,12 +1,13 @@
 class PostsController < ApplicationController
   def index
-    @user = User.find(params[:user_id])
-    @posts = @user.posts
+    @user = User.find(params[:user_id]) # Fetch the user by their ID
+    @posts = @user.posts # Fetch the posts related to the user
+    @comments = Comment.where(post_id: @posts.pluck(:id)) # Fetch comments related to these posts
   end
 
   def show
+    @user = User.find(params[:user_id])
     @post = Post.find(params[:id])
-    @user = @post.author
-    @comments = @post.comments.includes(:user)
+    @comments = @post.comments
   end
 end
