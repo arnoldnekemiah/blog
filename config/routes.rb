1c36ed230c3181b1f7d09ecada243920d3d29876
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-11-04 23:11:36 +0300

    added a form

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create]
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:create, :destroy] # Add comments resource
+    end
   end
 end
