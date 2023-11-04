4b6594c28f6e2c7a524e066312c58bf3beb2f12c
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-11-04 23:44:43 +0300

    added likes option

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :show] do
     resources :posts, only: [:index, :show, :new, :create] do
       resources :comments, only: [:create, :destroy] # Add comments resource
+      resources :likes, only: [:create]
     end
   end
 end
