79e2e1b54ca838f79036176554466e9c96528088
Ikonde Nekemiah Arnold <i-nad@localhost.localdomain>
2023-11-08 10:34:44 +0300

    corrected like and unlike action

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,12 @@
 Rails.application.routes.draw do
   root "users#index"
   resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show, :new, :create] do
-      resources :comments, only: [:create, :destroy]
-      member do
-        patch 'like', to: 'posts#like'
-        patch 'unlike', to: 'posts#unlike'
-      end
-    end
+  resources :posts, only: [:index, :show, :new, :create] do
+    resources :comments, only: [:create, :destroy]
+    patch 'like', on: :member
+    patch 'unlike', on: :member
   end
+end
+
   
 end
